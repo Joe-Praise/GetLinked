f7e36194cbb73fabfc3aefd9db36477855e5ed15
Joe Praise <alimipraisejoe@gmail.com>
2023-09-23 23:54:54 +0100

    Nav Scroll

--- src/Design/Navigation.js
+++ src/Design/Navigation.js
@@ -1,5 +1,7 @@
 import React, { useContext, useRef, useState } from "react";
+import * as Scroll from "react-scroll";
 import { NavLink } from "react-router-dom";
+
 import logo from "../images/getlinked.png";
 import ReactDOM from "react-dom";
 import { AppData } from "../Context";
@@ -8,6 +10,7 @@ import close from "../images/close btn.png";
 import openMenu from "../images/Vector (5).png";
 
 const Navigation = () => {
+  const Link = Scroll.Link;
   const menu = useRef();
   const ctx = useContext(AppData);
   const [, setCursorVariant] = ctx.getCursorVariant;
@@ -41,86 +44,103 @@ const Navigation = () => {
     },
     {
       name: "Timeline",
-      route: "/",
+      route: "timeline",
       className: "navitem",
     },
     {
       name: "Overview",
-      route: "/",
+      route: "overview",
       className: "navitem",
     },
     {
       name: "FAQs",
-      route: "/",
+      route: "faqs",
       className: "navitem",
     },
     {
       name: "Contact",
       route: "/contact",
       className: "navitem",
+      navLink: true,
     },
     {
       name: "Register",
       route: "/register",
       className: "RegisterBtn",
+      navLink: true,
     },
   ];
 
   return (
-    <nav className="nav">
-      <figure
-        className="logo"
-        onMouseEnter={() => textEnter(setCursorVariant, "text")}
-        onMouseLeave={() => textLeave(setCursorVariant, "default")}
-      >
-        <NavLink to="/">
-          <img src={logo} alt="Getlinked logo" />
-        </NavLink>
-      </figure>
-      <div className="openMenuContainer" onClick={openMenuHandler}>
-        <img src={openMenu} alt="open menu logo" />
-      </div>
-      <ul className="navNavLinksContainer" ref={menu}>
-        {links.map((el, i) => {
-          return el.onClick ? (
-            <li
-              className={el.className}
-              onClick={el.onClick}
-              key={el.id}
-              onMouseEnter={() => textEnter(setCursorVariant, "star")}
-              onMouseLeave={() => textLeave(setCursorVariant, "default")}
-            >
-              <img src={el.image} alt="x representing the close button" />
-            </li>
-          ) : (
-            <li
-              key={i}
-              onMouseEnter={() => textEnter(setCursorVariant, "link")}
-              onMouseLeave={() => textLeave(setCursorVariant, "default")}
-            >
-              <NavLink
-                className={({ isActive }) =>
-                  isActive ? `${el.className} active` : el.className
-                }
-                to={el.route}
-                end
-                onClick={closeMenuHandler}
+    <header>
+      <nav className="nav">
+        <figure
+          className="logo"
+          onMouseEnter={() => textEnter(setCursorVariant, "text")}
+          onMouseLeave={() => textLeave(setCursorVariant, "default")}
+        >
+          <NavLink to="/">
+            <img src={logo} alt="Getlinked logo" />
+          </NavLink>
+        </figure>
+        <div className="openMenuContainer" onClick={openMenuHandler}>
+          <img src={openMenu} alt="open menu logo" />
+        </div>
+        <ul className="navNavLinksContainer" ref={menu}>
+          {links.map((el, i) => {
+            return el.onClick ? (
+              <li
+                className={el.className}
+                onClick={el.onClick}
+                key={el.id}
+                onMouseEnter={() => textEnter(setCursorVariant, "star")}
+                onMouseLeave={() => textLeave(setCursorVariant, "default")}
               >
-                {el.name}
-              </NavLink>
-            </li>
-          );
-        })}
-        {modal && (
-          <React.Fragment>
-            {ReactDOM.createPortal(
-              <Backdrop />,
-              document.getElementById("overlay")
-            )}
-          </React.Fragment>
-        )}
-      </ul>
-    </nav>
+                <img src={el.image} alt="x representing the close button" />
+              </li>
+            ) : (
+              <li
+                key={i}
+                onMouseEnter={() => textEnter(setCursorVariant, "link")}
+                onMouseLeave={() => textLeave(setCursorVariant, "default")}
+              >
+                {el.navLink ? (
+                  <NavLink
+                    className={({ isActive }) =>
+                      isActive ? `${el.className} active` : el.className
+                    }
+                    to={el.route}
+                    end
+                    onClick={closeMenuHandler}
+                  >
+                    {el.name}
+                  </NavLink>
+                ) : (
+                  <Link
+                    className={el.className}
+                    to={el.route}
+                    smooth={true}
+                    offset={0}
+                    duration={1000}
+                    onClick={closeMenuHandler}
+                  >
+                    {el.name}
+                  </Link>
+                )}
+              </li>
+            );
+          })}
+          {modal && (
+            <React.Fragment>
+              {ReactDOM.createPortal(
+                <Backdrop />,
+                document.getElementById("overlay")
+              )}
+            </React.Fragment>
+          )}
+        </ul>
+      </nav>
+    </header>
   );
 };
 
